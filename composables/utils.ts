ff8b55cedee874ea4097abd705d23243f0b7ecb9
oussama goumghar <ogoumghar@sqli.com>
2025-06-12 10:57:42 +0100

    Fix 2 issues in composables/utils.ts

--- composables/utils.ts
+++ composables/utils.ts
@@ -12,17 +12,12 @@ export function formatDate(string: string) {
 export function formatTime(minutes: number) {
   // seconds
   const seconds = minutes * 60
-  let secondsLeft = seconds
 
   // hours
-  const hours = Math.floor(secondsLeft / 3600)
-  secondsLeft = secondsLeft % 3600
+  const hours = Math.floor(seconds / 3600)
+  const mins = Math.floor((seconds % 3600) / 60)
 
-  // mins
-  const mins = Math.floor(secondsLeft / 60)
-  secondsLeft = secondsLeft % 60
-
-  return `${hours ? `${hours}h ` : ''}${mins}min`
+  return `${hours ? hours + 'h ' : ''}${mins}min`
 }
 
 export function numberWithCommas(number: number) {
