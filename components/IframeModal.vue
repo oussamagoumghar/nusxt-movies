c0e2ae672cbf80630b83dff03bfd3ef8d2171d3e
oussama goumghar <ogoumghar@sqli.com>
2025-06-12 10:57:38 +0100

    Fix 1 issues in components/IframeModal.vue

--- components/IframeModal.vue
+++ components/IframeModal.vue
@@ -34,6 +34,7 @@ onClickOutside(el, () => {
       allow="autoplay; encrypted-media"
       allowfullscreen
       :src="src" w-full m5 lg:m20 border-none
+      title="Modal content"
     />
   </div>
 </template>
